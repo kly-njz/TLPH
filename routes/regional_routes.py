39c1830051c4cdb3591252748df801942fcac012
Cheezzyy1 <acapulcojohncedric66@gmail.com>
2026-02-22 23:24:55 -0800

    HAHAHAHHAHHAHAHAHAHAHA

--- routes/regional_routes.py
+++ routes/regional_routes.py
@@ -40,7 +40,7 @@ def transaction_view():
 
 @bp.route('/user-management-regional-list')
 @role_required('regional','regional_admin')
-def municipal_account_management_view():
+def regional_account_management_view():
     return render_template('regional/user-management-regional-list.html')
 
 @bp.route('/audit-logs-view')
@@ -52,4 +52,73 @@ def audit_logs_view():
 @role_required('regional','regional_admin')
 def application_view(application_id):
     return render_template('regional/application-regional-view.html')
-        
+
+@bp.route('/hrm/company')
+@role_required('regional','regional_admin')
+def company_view():
+    return render_template('regional/HR/company-regional.html')
+
+@bp.route('/hrm/departments')
+@role_required('regional','regional_admin')
+def departments_view():
+    return render_template('regional/HR/department-regional.html')
+
+@bp.route('/hrm/designations')
+@role_required('regional','regional_admin')
+def designations_view():
+    return render_template('regional/HR/designation-regional.html')
+
+@bp.route('/hrm/office-shifts')
+@role_required('regional','regional_admin')
+def office_shifts_view():
+    return render_template('regional/HR/office-shift-regional.html')
+
+@bp.route('/hrm/employees')
+@role_required('regional','regional_admin')
+def employees_view():
+    return render_template('regional/HR/employee-regional.html')
+
+@bp.route('/hrm/attendance')
+@role_required('regional','regional_admin')
+def attendance_view():
+    return render_template('regional/HR/attendance-regional.html')
+
+@bp.route('/hrm/holidays')
+@role_required('regional','regional_admin')
+def holidays_view():
+    return render_template('regional/HR/holiday-regional.html')
+
+@bp.route('/hrm/leave-requests')
+@role_required('regional','regional_admin')
+def leave_requests_view():
+    return render_template('regional/HR/leave-request-regional.html')
+
+@bp.route('/hrm/payroll-system')
+@role_required('regional','regional_admin')
+def payroll_system_view():
+    return render_template('regional/HR/payroll-regional.html')
+
+@bp.route('/accounting/accounting-dashboard')
+@role_required('regional','regional_admin')
+def accounting_dashboard_view():
+    return render_template('regional/accounting/dashboard-regional.html')
+
+@bp.route('/accounting/coa-templates')
+@role_required('regional','regional_admin')
+def accounting_coa_templates_view():
+    return render_template('regional/accounting/coa-templates-regional.html')
+
+@bp.route('/accounting/accounting-entities')
+@role_required('regional','regional_admin')
+def accounting_entities_view():
+    return render_template('regional/accounting/entities-regional.html')
+
+@bp.route('/accounting/accounting-deposits')
+@role_required('regional','regional_admin')
+def accounting_deposits_view():
+    return render_template('regional/accounting/deposit-category-regional.html')
+
+@bp.route('/accounting/accounting-expenses')
+@role_required('regional','regional_admin')
+def accounting_expenses_view():
+    return render_template('regional/accounting/expense-category-regional.html')
