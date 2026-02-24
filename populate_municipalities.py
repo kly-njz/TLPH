11fd82de924d8eeea47ed7bbd57710bd900c75bd
kly-njz <jimenez.jhoncarlo@minsu.edu.ph>
2026-02-23 18:32:03 -0800

    hala kalbooooooooooooooooooooooooooooooooooooooooo

--- populate_municipalities.py
+++ populate_municipalities.py
@@ -12,9 +12,18 @@ db = firestore.client()
 
 # Example region and municipalities (edit as needed)
 
-region = 'REGION IV-B'
+region = 'REGION-IV-B'
 municipalities = [
-    'Mamburao', 'San Jose', 'Sablayan', 'Abra de Ilog', 'Calintaan', 'Looc', 'Lubang', 'Paluan', 'Rizal', 'Sta. Cruz', 'Tilik'
+    # Occidental Mindoro
+    'Abra de Ilog', 'Calintaan', 'Looc', 'Lubang', 'Magsaysay', 'Mamburao', 'Paluan', 'Rizal', 'Sablayan', 'San Jose', 'Santa Cruz',
+    # Oriental Mindoro
+    'Baco', 'Bansud', 'Bongabong', 'Bulalacao', 'Gloria', 'Mansalay', 'Naujan', 'Pinamalayan', 'Pola', 'Puerto Galera', 'Roxas', 'San Teodoro', 'Socorro', 'Victoria',
+    # Marinduque
+    'Boac', 'Buenavista', 'Gasan', 'Mogpog', 'Santa Cruz', 'Torrijos',
+    # Romblon
+    'Alcantara', 'Banton', 'Cajidiocan', 'Calatrava', 'Concepcion', 'Corcuera', 'Ferrol', 'Looc', 'Magdiwang', 'Odiongan', 'Romblon', 'San Agustin', 'San Andres', 'San Fernando', 'San Jose', 'Santa Fe', 'Santa Maria',
+    # Palawan
+    'Aborlan', 'Agutaya', 'Araceli', 'Balabac', 'Bataraza', 'Brooke’s Point', 'Busuanga', 'Cagayancillo', 'Coron', 'Culion', 'Cuyo', 'Dumaran', 'El Nido', 'Kalayaan', 'Linapacan', 'Magsaysay', 'Narra', 'Puerto Princesa City', 'Quezon', 'Rizal', 'Roxas', 'San Vicente', 'Sofronio Española', 'Taytay'
 ]
 
 doc_ref = db.collection('municipalities').document(region)
