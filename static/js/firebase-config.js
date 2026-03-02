9f79d1bf8ff583a0830eccb20a394d946b12f0ee
kly-njz <jimenez.jhoncarlo@minsu.edu.ph>
2026-03-02 15:08:11 +0800

    hd

--- static/js/firebase-config.js
+++ static/js/firebase-config.js
@@ -1,21 +1,22 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { initializeApp, getApps } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCj3EnEG1XhF7_xWgt1vQK_VkT7288yd64",
-  authDomain: "denr-d02ae.firebaseapp.com",
-  projectId: "denr-d02ae",
-  storageBucket: "denr-d02ae.appspot.com",
-  messagingSenderId: "499245517370",
-  appId: "1:499245517370:web:c66598d7c86d5567a64303"
+  apiKey: "AIzaSyBpdvsvmF5JI7B50L8P_f9Q_OULRSCm9w4",
+  authDomain: "tlph-denr-system.firebaseapp.com",
+  projectId: "tlph-denr-system",
+  storageBucket: "tlph-denr-system.appspot.com",
+  messagingSenderId: "999028568640",
+  appId: "1:999028568640:web:2b488c8ea8e24ba39f19eb",
+  measurementId: "G-TXKQ1Z0J1X"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// Initialize Firebase safely (avoid duplicate-app errors)
+const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
